d5f22f8f9e94e41a022c672bbc64ce9c8556f92b
Aiden Gaul <100671205+aidengaul@users.noreply.github.com>
2023-05-23 23:06:39 -0400

    Solves 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    if (numOne < 0 || numTwo < 0 || typeof numOne !== "number" || typeof numTwo !== "number")
+        return "ERROR";
 
+    let sum = 0;
+    let lower = numOne;
+    let higher = numTwo;
+
+    if (numOne > numTwo) {
+        lower = numTwo;
+        higher = numOne;
+    }
+
+    for (let i = lower; i <= higher; i ++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
