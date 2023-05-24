83e4e0672bf9c8291969c5f9beb1c738a2b958c5
Aiden Gaul <100671205+aidengaul@users.noreply.github.com>
2023-05-23 22:56:32 -0400

    Solves 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    for (const target of args) {
+        const index = arr.indexOf(target);
 
+        if (index !== -1) {
+            arr.splice(index, 1);
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
